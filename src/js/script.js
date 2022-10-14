ab488e8e260e5f13efead84632ff8ef5d1eacb8e
Alexander <zhukoff.alexander@gmail.com>
2022-10-14 09:57:06 +0300

    feat: Create a responsive layout. Add a script
    Create a responsive layout for the fifth landing page. Add a tab switching script and a product description switching script.

--- src/js/script.js
+++ src/js/script.js
@@ -4,15 +4,17 @@ $(document).ready(function () {
         // adaptiveHeight: true,
         // autoplay: true,
         // autoplaySpeed: 2000,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"/></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"/></button>',
+        prevArrow:
+            '<button type="button" class="slick-prev"><img src="icons/left.png"/></button>',
+        nextArrow:
+            '<button type="button" class="slick-next"><img src="icons/right.png"/></button>',
         responsive: [
             {
-              breakpoint: 768,
-              settings: {
-                dots: true,
-                arrows: false
-              }
+                breakpoint: 768,
+                settings: {
+                    dots: true,
+                    arrows: false,
+                },
             },
             // {
             //   breakpoint: 600,
@@ -31,6 +33,34 @@ $(document).ready(function () {
             // You can unslick at a given breakpoint now by adding:
             // settings: "unslick"
             // instead of a settings object
-          ],
+        ],
     });
+    $('ul.catalog__tabs').on(
+        'click',
+        'li:not(.catalog__tab_active)',
+        function () {
+            $(this)
+                .addClass('catalog__tab_active')
+                .siblings()
+                .removeClass('catalog__tab_active')
+                .closest('div.container')
+                .find('div.catalog__content')
+                .removeClass('catalog__content_active')
+                .eq($(this).index())
+                .addClass('catalog__content_active');
+        }
+    );
+
+
+    function toggleSlide(item) {
+      $(item).each(function (i) {
+        $(this).on('click', function (e) {
+            e.preventDefault();
+            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active')
+            $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active')
+        });
+    });
+    }
+    toggleSlide('.catalog-item__link')
+    toggleSlide('.catalog-item__back')
 });
