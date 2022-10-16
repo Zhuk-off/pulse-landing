dbb0c35cf44a73176001744129e3ca38f7106e47
Alexander <zhukoff.alexander@gmail.com>
2022-10-16 09:27:23 +0300

    feat: Create modal windows and JQuery script

--- src/js/script.js
+++ src/js/script.js
@@ -51,16 +51,37 @@ $(document).ready(function () {
         }
     );
 
-
     function toggleSlide(item) {
-      $(item).each(function (i) {
-        $(this).on('click', function (e) {
-            e.preventDefault();
-            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active')
-            $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active')
+        $(item).each(function (i) {
+            $(this).on('click', function (e) {
+                e.preventDefault();
+                $('.catalog-item__content')
+                    .eq(i)
+                    .toggleClass('catalog-item__content_active');
+                $('.catalog-item__list')
+                    .eq(i)
+                    .toggleClass('catalog-item__list_active');
+            });
+        });
+    }
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
+
+    // Modal
+    $('[data-madal="consultation"]').on('click', function () {
+        $('.overlay, #consultation').fadeIn();
+    });
+
+    $('.modal__close').on('click', function () {
+        $('.overlay, #consultation, #order, #thanks').fadeOut();
+    });
+
+    $('.button_mini').each(function (i) {
+        $(this).on('click', function () {
+            $('#order .modal__descr').text(
+                $('.catalog-item__subtitle').eq(i).text()
+            );
+            $('.overlay,  #order').fadeIn();
         });
     });
-    }
-    toggleSlide('.catalog-item__link')
-    toggleSlide('.catalog-item__back')
 });
