b6d87060653df5fca5908b2fc3fb2935b5c72d62
Alexander <zhukoff.alexander@gmail.com>
2022-10-19 12:14:56 +0300

    feat: Add up button and smooth scroll

--- src/js/script.js
+++ src/js/script.js
@@ -121,4 +121,37 @@ $(document).ready(function () {
         });
         return false;
     });
+
+    // scroll
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $('.pageup').on('click', function (event) {
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== '') {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            const hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate(
+                {
+                    scrollTop: $(hash).offset().top,
+                },
+                800,
+                function () {
+                    // Add hash (#) to URL when done scrolling (default click behavior)
+                    window.location.hash = hash;
+                }
+            );
+        } // End if
+    });
 });
